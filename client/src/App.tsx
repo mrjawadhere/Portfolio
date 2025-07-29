c7b090bc88a2429d084565a980542ae1891caf6f
mrjawadhere <mrjawadhere@gmail.com>
2025-07-30 01:25:55 +0500

    commit name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,35 @@
-import { Switch, Route } from "wouter";
+// import { Switch, Route } from "wouter";
+// import { queryClient } from "./lib/queryClient";
+// import { QueryClientProvider } from "@tanstack/react-query";
+// import { Toaster } from "@/components/ui/toaster";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import NotFound from "@/pages/not-found";
+// import HomePage from "@/pages/HomePage";
+
+// function Router() {
+//   return (
+//     <Switch>
+//       <Route path="/" component={HomePage} />
+//       {/* Fallback to 404 */}
+//       <Route component={NotFound} />
+//     </Switch>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <TooltipProvider>
+//         <Toaster />
+//         <Router />
+//       </TooltipProvider>
+//     </QueryClientProvider>
+//   );
+// }
+
+// export default App;
+
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,12 +37,12 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import HomePage from "@/pages/HomePage";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
-      <Route path="/" component={HomePage} />
+      <Route path="/" children={<HomePage />} />
       {/* Fallback to 404 */}
-      <Route component={NotFound} />
+      <Route children={<NotFound />} />
     </Switch>
   );
 }
@@ -21,7 +52,9 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <Router base="/Portfolio">
+          <AppRouter />
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
